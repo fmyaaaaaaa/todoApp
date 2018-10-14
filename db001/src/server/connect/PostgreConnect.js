05bea46adf406615130aeb96f35b1951e4f240a4
fmyaaa <fmyaaaaaaa@yahoo.co.jp>
2018-10-14 17:38:14 +0900

    ◆Todoの状態更新のAPIをPUTに修正 →WebAPIの規則に違反していたため、MethodをGETからPUTへ。 →それにともない、「Access-Control-Allow-Methods」を追加。 　PUT.DELETEのアクセスを許可するため。

--- db001/src/server/connect/PostgreConnect.js
+++ db001/src/server/connect/PostgreConnect.js
@@ -9,6 +9,7 @@ const userList = require('../middlle/UserList');
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
 });
 
--- db001/src/server/middlle/TodoList.js
+++ db001/src/server/middlle/TodoList.js
@@ -12,7 +12,7 @@ router.get("/todo/:userId", async (req, res) => {
 });
 
 // UserのTodoの状態を変更
-router.get("/updateTodo/:todoId", async (req, res) => {
+router.put("/updateTodo/:todoId", async (req, res) => {
     const {todoId} = req.params;
     const {rows} = await db.query("UPDATE TODO_LIST set isdone = TRUE WHERE id = $1", [todoId]);
     res.send(rows);
